bab7cc8cad3be711567c9c0dff0b22dd2ef58f9b
Wei Chen <ipondering.weic@gmail.com>
2019-04-20 10:33:08 -0700

    Add training

---
+++ xgboost_train.py
@@ -0,0 +1,29 @@
+import sys
+import time
+
+import xgboost as xgb
+
+def main():
+    train_data = sys.argv[1]
+    test_data = sys.argv[2]
+
+    # read in data
+    dtrain = xgb.DMatrix(train_data)
+    dtest = xgb.DMatrix(test_data)
+
+    # specify parameters via map
+    param = {'max_depth':2, 'eta':1, 'silent':1 }
+    num_round = 2
+
+    print("Training")
+    bst = xgb.train(param, dtrain, num_round)
+    # make prediction
+
+    print("Predicting")
+    start = time.time()
+    preds = bst.predict(dtest)    
+    total = time.time() - start
+    print("{} seconds {}".format(total, preds))
+
+if __name__ == '__main__':
+    main()
